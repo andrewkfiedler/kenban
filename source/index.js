9f5b4c7bc8fc09bfb476dd99bd0e323582344e1a
Andrew Fiedler <andrewkfiedler@gmail.com>
2018-08-24 12:50:29 -0700

    moving towards hotkeys

--- source/index.js
+++ source/index.js
@@ -1,26 +1,7 @@
 import './index.scss';
 
 function decorateThePage() {
-    document.querySelectorAll('#additional-content li, #all-info').forEach((element) => {
-        element.tabIndex = 0;
-        element.onkeydown = (e) => {
-            e.stopPropagation();
-            if (e.keyCode === 32 || e.keyCode === 13) {
-                element.click();
-            }
-        } 
-    })
-    document.querySelectorAll('#information').forEach((element) => {
-        element.onkeydown = (e) => {
-            if (e.target.href !== undefined) {
-                e.stopPropagation();
-                if (e.keyCode === 32 || e.keyCode === 13) {
-                    e.target.click();
-                }
-            }
-        } 
-    })
-    document.querySelector('.audio-btn').tabIndex = '-1';  // can already select the li now
+    
 }
 
 function waitForPageLoad() {
