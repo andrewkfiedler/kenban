e71c30613db6c1dc3beb579620b251dfcd656596
Andrew Fiedler <andrewkfiedler@gmail.com>
2018-08-24 13:09:00 -0700

    Update to keyboard shortcuts

--- source/index.js
+++ source/index.js
@@ -1,17 +1,1 @@
 import './index.scss';
-
-function decorateThePage() {
-    
-}
-
-function waitForPageLoad() {
-    if (document.querySelectorAll('#information').length === 0) {
-        setTimeout(() => {
-            waitForPageLoad();
-        }, 1000);
-    } else {
-        decorateThePage();
-    }
-}
-
-waitForPageLoad();
