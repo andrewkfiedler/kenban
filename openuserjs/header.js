5673858424e9989a344d670f318f5280944934dd
Andrew Fiedler <andrewkfiedler@gmail.com>
2018-08-24 13:59:26 -0700

    Add openuserjs generation for postbuild publishing

---
+++ openuserjs/header.js
@@ -0,0 +1,8 @@
+// @name        Kenban
+// @namespace   andrewkfiedler
+// @description Adds keyboard hints to be inline with their respective buttons.
+// @version     1.0.0
+// @include     https://www.wanikani.com/review/session*
+// @run-at      document-end
+// @grant       none
+
